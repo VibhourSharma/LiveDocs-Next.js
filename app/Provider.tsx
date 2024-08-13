db03b21fc3c4f9923ed96c94c66e1509c6bce237
Vibhour Sharma <vibhour121@gmail.com>
2024-08-13 19:32:53 +0530

    🆕feat: edit document title

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -8,7 +9,14 @@ import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+      authEndpoint="/api/liveblocks-auth"
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
